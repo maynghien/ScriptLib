83095367484e9813d3a8dde893d01498be6495ee
maynghien <49431183+maynghien@users.noreply.github.com>
2021-07-26 07:05:48 +0700

    wwwroot

---
+++ wwwroot/lib/jqueryuicombobox/customcombobox.js
@@ -0,0 +1,188 @@
+﻿$(function () {
+    $.widget("custom.combobox", {
+        _create: function () {
+            this.wrapper = $("<span>")
+              .addClass("custom-combobox")
+              .insertAfter(this.element);
+
+            this.element.hide();
+            this._createAutocomplete();
+            this._createShowAllButton();
+        },
+
+        _createAutocomplete: function () {
+            var selected = this.element.children(":selected"),
+              value = selected.val() ? selected.text() : "";
+
+            this.input = $("<input>")
+              .appendTo(this.wrapper)
+              .val(value)
+              .attr("title", "")
+              .addClass("custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left")
+              .autocomplete({
+                  delay: 0,
+                  minLength: 0,
+                  source: $.proxy(this, "_source")
+              })
+              .tooltip({
+                  classes: {
+                      "ui-tooltip": "ui-state-highlight"
+                  }
+              });
+
+            this._on(this.input, {
+                autocompleteselect: function (event, ui) {
+                    ui.item.option.selected = true;
+                    this._trigger("select", event, {
+                        item: ui.item.option
+                    });
+
+                    this._trigger("change", event, {
+                        item: ui.item.option
+                    });
+                    this.element.trigger("change");
+                },
+
+                autocompletechange: "_removeIfInvalid"
+            });
+        },
+
+        _createShowAllButton: function () {
+            var input = this.input,
+              wasOpen = false;
+
+            this.a =$("<a>")
+              .attr("tabIndex", -1)
+              //.attr("title", "Hiển thị tất cả")
+              .tooltip()
+                .appendTo(this.wrapper)
+                .append("<i class='fas fa-caret-down'></i>")
+              .button({
+                  icons: {
+                      primary: "fas fa-caret-down"
+                  },
+                  text: false
+              })
+              .removeClass("ui-corner-all")
+              .addClass("custom-combobox-toggle ui-corner-right")
+              .on("mousedown", function () {
+                  wasOpen = input.autocomplete("widget").is(":visible");
+              })
+              .on("click", function () {
+                  input.trigger("focus");
+
+                  // Close if already visible
+                  if (wasOpen) {
+                      return;
+                  }
+
+                  // Pass empty string as value to search for, displaying all results
+                  input.autocomplete("search", "");
+              });
+        },
+
+        _source: function (request, response) {
+            var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
+            response(this.element.children("option").map(function () {
+                var text = $(this).text();
+                if (this.value && (!request.term || matcher.test(text)))
+                    return {
+                        label: text,
+                        value: text,
+                        option: this
+                    };
+            }));
+        },
+
+        _removeIfInvalid: function (event, ui) {
+
+            // Selected an item, nothing to do
+            if (ui.item) {
+                return;
+            }
+
+            // Search for a match (case-insensitive)
+            var value = this.input.val(),
+              valueLowerCase = value.toLowerCase(),
+              valid = false;
+            this.element.children("option").each(function () {
+                if ($(this).text().toLowerCase() === valueLowerCase) {
+                    this.selected = valid = true;
+                    return false;
+                }
+            });
+
+            // Found a match, nothing to do
+            if (valid) {
+                return;
+            }
+
+            // Remove invalid value
+            this.input
+              .val("")
+              .attr("title", " Không tìm thấy " + value + ", vui lòng kiểm tra lại")
+              .tooltip("open");
+            this.element.val("");
+            this._delay(function () {
+                this.input.tooltip("close").attr("title", "");
+            }, 2500);
+            this.input.autocomplete("instance").term = "";
+        },
+
+        _destroy: function () {
+            this.wrapper.remove();
+            this.element.show();
+        },
+        autocomplete: function (value) {
+            this.element.val(value);
+            var selectedoption = this.element[0].options[this.element[0].selectedIndex];
+            if (selectedoption != null) {
+                var text = selectedoption.text;
+                this.input.val(text);
+            }
+            this.element.trigger("change");
+        },
+        select: function (event, ui) {
+            
+            this.element.trigger("change");
+        },
+        disable: function () {
+            this.input.prop('disabled', true);
+            this.input.autocomplete("disable");
+            this.a.hide();
+        },
+        enable: function () {
+            this.input.prop('disabled', false);
+            this.input.autocomplete("enable");
+            this.a.show();
+        }
+    });
+    $(document).ready(function () {
+        $("select").not(".ui-pg-selbox").combobox();
+    })
+    $(function () {
+        $.widget("custom.catcomplete", $.ui.autocomplete, {
+            _create: function () {
+                this._super();
+                this.widget().menu("option", "items", "> :not(.ui-autocomplete-category)");
+            },
+            _renderMenu: function (ul, items) {
+                var that = this,
+                  currentCategory = "";
+                $.each(items, function (index, item) {
+                    var li;
+                    if (item.category != currentCategory) {
+                        ul.append("<li class='ui-autocomplete-category'>" + item.category + "</li>");
+                        currentCategory = item.category;
+                    }
+                    li = that._renderItemData(ul, item);
+                    if (item.category) {
+                        li.attr("aria-label", item.category + " : " + item.label);
+                    }
+                });
+            }
+        });
+
+    });
+
+});
